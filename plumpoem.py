c7c2f10c8394e1cc575d85fc65ace7d7fac12267
Robin Camille Davis <robincamilledavis@gmail.com>
2017-12-04 13:15:38 -0500

    Added sample output

--- plumpoem.py
+++ plumpoem.py
@@ -4,12 +4,13 @@
 # tweets one variant of This Is Just To Say by William Carlos Williams
 # original: https://www.poetryfoundation.org/poems/56159/this-is-just-to-say
 
-import json, io, time, urllib2, tweepy
+import json, io, time, urllib2
 from random import randint
 from credentials import *
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
-api = tweepy.API(auth)
+##import tweepy
+##auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+##auth.set_access_token(ACCESS_TOKEN, ACCESS_SECRET)
+##api = tweepy.API(auth)
 
 fruit_file = urllib2.urlopen('https://raw.githubusercontent.com/dariusk/corpora/master/data/foods/fruits.json')
 fruit_list = fruit_file.read()
@@ -32,10 +33,10 @@ colors = json.loads(colors_list)['colors']
 
     
 # Pick random numbers
-fruit_num = randint(0, len(fruits))
-room_num = randint(0, len(rooms))
-adjectives_num = randint(0, len(adjectives))
-color_num = randint(0, len(colors))
+fruit_num = randint(0, len(fruits)-1)
+room_num = randint(0, len(rooms)-1)
+adjectives_num = randint(0, len(adjectives)-1)
+color_num = randint(0, len(colors)-1)
 
 # Choose random items from each list using random numbers
 fruit_chosen = fruits[fruit_num].lower() #should be pluralized here but pluralizing is hard
@@ -47,6 +48,22 @@ adjective_chosen = adjectives[adjectives_num].lower()
 poem = 'I have eaten\nthe %ss\nthat were in\nthe %s\n\nand which\nyou were probably\nsaving\nfor breakfast\n\nForgive me\nthey were delicious\nso %s\nand so %s' \
        % (fruit_chosen, room_chosen, color_chosen, adjective_chosen)
 
-api.update_status(poem)
-#print poem
+#api.update_status(poem)
+print poem
 
+## SAMPLE OUTPUT
+
+##I have eaten
+##the apricots
+##that were in
+##the storm cellar
+##
+##and which
+##you were probably
+##saving
+##for breakfast
+##
+##Forgive me
+##they were delicious
+##so black
+##and so hopeless
