4d5e48a89a01404590cc54d717a2dfb6390f464b
Robin Camille Davis <rdvs18@gmail.com>
2020-07-07 17:14:20 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,5 @@
+# Run this code to make sure your Python environment is A-OK.
+# No need to change anything.
+# You should see "Hello, there!" printed in your console (output screen).
+
+print("Hello, there!")
